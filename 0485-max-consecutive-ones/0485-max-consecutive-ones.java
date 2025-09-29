80a334fdd134bd2c93223e44fc3567e712010314
Rishendra.s <rishendrababu46@gmail.com>
2025-09-29 22:48:57 +0530

    0485-max-consecutive-ones .java Easy 09-29-2025 50.2 MB (48.06%) 3 ms (58.09%)

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+      int cnt=0;
+      int maxi=0;
+      for(int i=0;i<nums.length;i++){
+        if(nums[i]==1){
+            cnt ++;
+        }else{
+            cnt=0;
+        }maxi=Math.max(maxi,cnt);
+      }return maxi;
+        
+    }
+}
