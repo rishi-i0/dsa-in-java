c0ee8b1b7f9c3398630e5fe281464cdc3776c7ea
Rishendra.s <rishendrababu46@gmail.com>
2025-09-30 22:22:35 +0530

    0136-single-number .java Easy 09-30-2025 46.5 MB (27.42%) 1 ms (99.87%)

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         int n = nums.length;
+
+        
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+    }
+
+    }
