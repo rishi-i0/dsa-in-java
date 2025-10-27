6bcfb26095f7ccd1d94ca044b4f42083db75d278
Rishendra.s <rishendrababu46@gmail.com>
2025-10-27 23:05:14 +0530

    0038-count-and-say .java Medium 10-27-2025 41.4 MB (64.92%) 7 ms (46.05%)

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        
+        String s = "1";
+        
+        for (int i = 2; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(j) == s.charAt(j - 1)) {
+                    count++;
+                } else {
+                    sb.append(count).append(s.charAt(j - 1));
+                    count = 1;
+                }
+            }
+            sb.append(count).append(s.charAt(s.length() - 1));
+            s = sb.toString();
+        }
+        
+        return s;
+    }
+}
