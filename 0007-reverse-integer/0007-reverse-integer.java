ed5c53a81a1b1a8312096d8ccc2b6b816c3b1631
Rishendra.s <rishendrababu46@gmail.com>
2026-01-03 23:15:32 +0530

    0007-reverse-integer .java Medium 01-03-2026 42.9 MB (5.82%) 1 ms (99.76%)

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int reverse(int x) {
-        int rev=0;
-        while(x!=0){
-          int  ld=x%10;
+    public int reverse(int n) {
+      int rev = 0;
+      while(n!=0){
+       int ld=n%10;
+        n=n / 10;
+       
+         if (rev > Integer.MAX_VALUE / 10 ||
+           (rev == Integer.MAX_VALUE / 10 && ld > 7)){
+        return 0;
+       }
+       if(rev<Integer.MIN_VALUE/10 || (rev==Integer.MIN_VALUE && ld<-8)){
+        return 0 ;
+       }
+       rev=rev*10+ld;
 
-         if(rev>Integer.MAX_VALUE/10 || rev==Integer.MAX_VALUE/10&&ld>7){
-            return 0;
-         }
-         if(rev<Integer.MIN_VALUE/10 || rev==Integer.MIN_VALUE/10&&ld<-8){
-            return 0;
-         }
-         x=x/10;
-         rev=rev*10+ld;
-         
-
-        }return rev;
+      }return rev;
 }
 }
