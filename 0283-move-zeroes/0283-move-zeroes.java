9d44ceae45c71871405184fb91ce95e7dab83b20
Rishendra.s <rishendrababu46@gmail.com>
2026-01-10 14:13:54 +0530

    0283-move-zeroes .java Easy 01-10-2026 47.7 MB (77.59%) 2 ms (89.25%)

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,41 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int j = -1;
-        
-        // find first zero
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                break;
-            }
+      int l=0;
+      for(int r=0;r<nums.length;r++){
+        if(nums[r]!=0){
+            int temp=nums[r];
+            nums[r]=nums[l];
+            nums[l]=temp;
+            l++;
         }
+      }
         
-        // no zero found
-        if (j == -1) return;
-
-        // move non-zero elements forward
-        for (int i = j + 1; i < n; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        int nums[] = {1, 0, 2, 3, 20, 0, 4, 8, 1};
-        
-       
-        Solution obj = new Solution();
-        obj.moveZeroes(nums);
-
-        // print result
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + " ");
-        }
-        System.out.println();
     }
 }
