dda356db93c77efb6461f2501d888a455c5a9784
Rishendra.s <rishendrababu46@gmail.com>
2026-01-11 14:41:26 +0530

    0121-best-time-to-buy-and-sell-stock .java Easy 01-11-2026 94.7 MB (6.39%) 1 ms (99.72%)

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,26 +1,16 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length < 2) return 0;
-
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
-
-        for (int price : prices) {
-            if (price < minPrice) {
-                minPrice = price;
-            } else {
-                int profit = price - minPrice;
-                if (profit > maxProfit) {
-                    maxProfit = profit;
-                }
+        int buy=prices[0];
+        int profit=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }else{
+                int cur_profit=prices[i]-buy;
+                profit=Math.max(cur_profit,profit);
             }
-        }
-        return maxProfit;
+
+        }return profit;
     }
 
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.maxProfit(new int[]{7,1,5,3,6,4}));
-        System.out.println(s.maxProfit(new int[]{7,6,4,3,1}));
-    }
 }
