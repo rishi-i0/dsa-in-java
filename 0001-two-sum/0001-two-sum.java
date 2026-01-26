d4a0138e4ce7dbff22024f4c01d1b7c6432615e9
Rishendra.s <rishendrababu46@gmail.com>
2026-01-26 13:58:39 +0530

    0001-two-sum .java Easy 01-26-2026 47.3 MB (30.78%) 2 ms (99.12%)

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,20 @@
 import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n=nums.length;
-     int[] ans = new int[2];
-        ans[0] = ans[1] = -1;
-        HashMap<Integer, Integer> mpp = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int moreNeeded = target - num;
-            if (mpp.containsKey(moreNeeded)) {
-                ans[0] = mpp.get(moreNeeded);
-                ans[1] = i;
-                return ans;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
             }
 
-            mpp.put(nums[i], i);
+            map.put(nums[i], i);
         }
-        return ans;       
+
+        return new int[]{};
     }
 }
