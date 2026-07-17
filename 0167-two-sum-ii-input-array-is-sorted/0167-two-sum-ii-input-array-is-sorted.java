8584633105d8b9549162e3b608efdd761dc58296
Rishendra.s <rishendrababu46@gmail.com>
2026-07-17 15:39:06 +0530

    0167-two-sum-ii-input-array-is-sorted .java Medium 07-17-2026 48.4 MB (42.39%) 2 ms (95.65%)

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -5,14 +5,13 @@ class Solution {
         while(l<r){
             int sum=numbers[l]+numbers[r];
             if(sum==target){
-                return new int[]{l+1,r+1};
-            }
-            else if (sum>target){
+                return new int[] {l+1,r+1};
+            }else if(sum<target){
+                l++;
+            }else{
                 r--;
             }
-            else{
-                l++;
-            }
-        } return new int[]{};
+        } return new int[] {};
+      
     }
 }
