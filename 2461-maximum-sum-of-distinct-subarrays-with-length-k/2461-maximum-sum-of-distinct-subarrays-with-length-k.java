557a70368b358c38779aa44b44917aef0b8d9a7a
Rishendra.s <rishendrababu46@gmail.com>
2026-07-18 23:12:16 +0530

    2461-maximum-sum-of-distinct-subarrays-with-length-k .java Medium 07-18-2026 98.5 MB (81.02%) 38 ms (72.04%)

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,22 +1,22 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        Set<Integer> set=new HashSet<>();
-        long curSum=0 , maxSum=0;
-        int L=0;
-        for(int R=0;R<nums.length;R++){
-          while(set.contains(nums[R]) || set.size()==k){ 
-            set.remove(nums[L]);
-            curSum-=nums[L];
-            L++;
-          }
-          curSum+=nums[R]; 
-          set.add(nums[R]);
-          if(set.size()==k){
+      Set<Integer> set= new HashSet<>();
+      long curSum=0;
+      long maxSum=0;
+      int left=0;
+      for(int right=0; right<nums.length;right++){
+        while(set.contains(nums[right])||set.size()==k){
+            set.remove(nums[left]);
+            curSum-=nums[left];
+            left++;
+        }
+        curSum+=nums[right];
+        set.add(nums[right]);
+        if(set.size()==k){
             maxSum=Math.max(curSum,maxSum);
-          }
+        }
+      } return maxSum;
 
-
-        }return maxSum;
 
     }
 }
