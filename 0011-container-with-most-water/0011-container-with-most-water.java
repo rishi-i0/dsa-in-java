59769b16e8ce8041cff37fab9bad016092af4e1b
Rishendra.s <rishendrababu46@gmail.com>
2026-07-21 16:27:23 +0530

    0011-container-with-most-water .java Medium 07-21-2026 77.3 MB (49.27%) 5 ms (82.67%)

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0;
+        int r=height.length-1;
+        int maxArea=0;
+        while(l<r){
+            int width=r-l;
+            int h=Math.min(height[l],height[r]);
+            maxArea=Math.max(maxArea,width*h);
+            if(height[l]<height[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }return maxArea;
+    }
+}
